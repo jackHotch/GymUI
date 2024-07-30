19eeb4a419b6b6c01e445fabcbf8030110c75e47
jackHotch <jhotchkiss2021@gmail.com>
2024-07-29 21:54:29 -0400

    added terser and finished lib setup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,42 +1,41 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-
-import packageJson from "./package.json" assert { type: "json" };
-
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import postcss from 'rollup-plugin-postcss'
+import packageJson from './package.json' assert { type: 'json' }
+import terser from '@rollup/plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({tsconfig: "./tsconfig.json"}),
-            postcss({
-                plugins: [],
-                modules: true
-              }),
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-        external: [/\.(css|less|scss)$/],
-    },
-
-];
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss({
+        plugins: [],
+        modules: true,
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
+]
